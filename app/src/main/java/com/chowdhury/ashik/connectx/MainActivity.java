3355fe77e17d3804a29ab4a62ea1976d1e2e6a19
AshikChowdhury <ashikbracu@gmail.com>
2019-10-31 15:26:13 +0600

    added messages for winner and draw situation

--- app/src/main/java/com/chowdhury/ashik/connectx/MainActivity.java
+++ app/src/main/java/com/chowdhury/ashik/connectx/MainActivity.java
@@ -4,10 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView winnerMessage;
+    LinearLayout layout;
+    GridLayout gridLayout;
+    boolean gameIsActive = true;
     // 0 Equals Yeallow
     int activePlayer = 0;
     // 2 means unplayed
@@ -19,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        winnerMessage = findViewById(R.id.winnerMessage);
+        layout = findViewById(R.id.playAgainLayout);
+        gridLayout = findViewById(R.id.gridLayout);
     }
 
     public void dropIn(View view) {
@@ -26,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
         int tabCounter = Integer.parseInt(counter.getTag().toString());
 
-        if (gameState[tabCounter] == 2){
+        if (gameState[tabCounter] == 2 && gameIsActive){
             gameState[tabCounter] = activePlayer;
             counter.setTranslationY(-1000f);
             if (activePlayer == 0){
@@ -41,13 +50,39 @@ public class MainActivity extends AppCompatActivity {
                 if (gameState[winningPosition[0]] == gameState[winningPosition[1]] &&
                         gameState[winningPosition[1]] == gameState[winningPosition[2]] &&
                         gameState[winningPosition[0]] != 2 ){
-                    System.out.println(gameState[winningPosition[0]]);
 
+                    //Someone has win!!
+                    gameIsActive = false;
+                    String winner = "Red";
+                    if (gameState[winningPosition[0]] == 0){
+                      winner = "Yellow";
+                    }
+                    winnerMessage.setText(winner+" Has Won!");
+                    layout.setVisibility(View.VISIBLE);
+
+                }else{
+                    boolean gameIsOver = true;
+                    for (int counterState : gameState){
+                        if (counterState == 2) gameIsOver = false;
+                    }
+                    if (gameIsOver){
+                        winnerMessage.setText("It's A Draw!");
+                        layout.setVisibility(View.VISIBLE);
+                    }
                 }
             }
         }
     }
 
     public void playAgain(View view) {
+       layout.setVisibility(View.INVISIBLE);
+       activePlayer = 0;
+       gameIsActive = true;
+       for (int i = 0; i<gameState.length; i++){
+           gameState[i] = 2;
+       }
+       for (int i = 0; i<gridLayout.getChildCount(); i++){
+           ((ImageView) gridLayout.getChildAt(i)).setImageResource(0);
+       }
     }
 }
